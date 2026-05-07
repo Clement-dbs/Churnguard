e13d400aecdd3afe9aadec841c1d5d0a6d4bc11c
clement <clement.duboisdbs@gmail.com>
2026-05-07 13:48:48 +0200

    Ruff format

--- churnguard/main.py
+++ churnguard/main.py
@@ -1,7 +1,7 @@
 from churnguard.data import load_data, preprocess
 from churnguard.train import train_model
 from churnguard.evaluate import compute_metrics
-import mlflow
+
 
 def main():
     df = load_data("./data/telco_churn.csv")
@@ -9,6 +9,7 @@ def main():
 
     model = train_model(X_train, X_test, y_train, y_test, preprocessed)
     compute_metrics(model, X_test, y_test)
+
 
 if __name__ == "__main__":
     main()
