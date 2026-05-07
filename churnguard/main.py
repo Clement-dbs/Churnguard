a4c20de55c9c8349023dba1eff9f9cc257e023c1
clement <clement.duboisdbs@gmail.com>
2026-05-07 13:46:18 +0200

    main training execution file

---
+++ churnguard/main.py
@@ -0,0 +1,14 @@
+from churnguard.data import load_data, preprocess
+from churnguard.train import train_model
+from churnguard.evaluate import compute_metrics
+import mlflow
+
+def main():
+    df = load_data("./data/telco_churn.csv")
+    X_train, X_test, y_train, y_test, preprocessed = preprocess(df)
+
+    model = train_model(X_train, X_test, y_train, y_test, preprocessed)
+    compute_metrics(model, X_test, y_test)
+
+if __name__ == "__main__":
+    main()
