3a648d1f833756a53ca7c6f9d67947f9aa0eefd4
clement <clement.duboisdbs@gmail.com>
2026-05-07 09:59:30 +0200

    Remove unused imports and pyproject file

--- churnguard/data.py
+++ churnguard/data.py
@@ -1,7 +1,6 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
-from sklearn.pipeline import Pipeline
 from sklearn.model_selection import train_test_split
 
 def load_data(path:str) -> pd.DataFrame:
